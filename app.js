2ffcae6d3667541df81325cf0c18bce6b002723c
jksison <johnk.sison@gmail.com>
2019-01-28 23:47:16 -0500

    added lose logic

--- app.js
+++ app.js
@@ -34,16 +34,34 @@ guessBtn.addEventListener('click', function() {
    }
 
    if (guess === winningNum) {
-      guessInput.disabled = true;
-      guessInput.style.borderColor = 'green';
-      setMessage(`${guess} is correct, YOU WIN!`, 'green');
+      gameOver(true, `${guess} is correct, YOU WIN!`);
    } else {
+      guessesLeft -= 1;
 
+      if (guessesLeft === 0) {
+         gameOver(false, `Game over, you lost. The correct number was ${winningNum}.`);
+      } else {
+         guessInput.style.borderColor = 'red';
+         guessInput.value = '';
+
+         setMessage(`${guess} is not correct, ${guessesLeft} guesses left.`);
+      }
    }
 });
 
-function setMessage(msg, color) {
+function gameOver(won, msg) {
+   let color;
+   won === true ? color = 'green' : color = 'red';
+
+   guessInput.disabled = true;
+   guessInput.style.borderColor = color;
    message.style.color = color;
+   setMessage(msg, color);
+}
+
+
+
+function setMessage(msg) {
    message.textContent = msg;
 }
 
